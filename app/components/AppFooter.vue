3aad70a661e9be65eb7ec267ab354987e1ef6ed4
RaulSAraujo <raul.araujo.2000@hotmail.com>
2026-01-28 10:39:44 -0300

    ✨ feat: criando app footer

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const { footer } = useAppConfig()
+</script>
+
+<template>
+  <UFooter :ui="{ left: 'text-muted text-xs' }">
+    <template #left>
+      {{ footer.credits }}
+    </template>
+
+    <template #right>
+      <template v-if="footer?.links">
+        <UButton
+          v-for="(link, index) of footer?.links"
+          :key="index"
+          v-bind="{ size: 'xs', color: 'neutral', variant: 'ghost', ...link }"
+        />
+      </template>
+    </template>
+  </UFooter>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,6 +5,7 @@
     <UContainer class="sm:border-x border-default pt-10">
       <AppHeader :links="navLinks" />
       <slot />
+      <AppFooter />
     </UContainer>
   </div>
 </template>
