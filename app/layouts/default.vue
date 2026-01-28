2e84dcb044e83c87a833e8a7beffd1e3bfbee329
RaulSAraujo <raul.araujo.2000@hotmail.com>
2026-01-28 10:23:44 -0300

    ✨ feat: criando app header

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,6 +3,7 @@
 <template>
   <div>
     <UContainer class="sm:border-x border-default pt-10">
+      <AppHeader :links="navLinks" />
       <slot />
     </UContainer>
   </div>
---
+++ app/utils/links.ts
@@ -0,0 +1,9 @@
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+export const navLinks: NavigationMenuItem[] = [
+  {
+    label: 'Home',
+    icon: 'i-lucide-home',
+    to: '/'
+  }
+]
